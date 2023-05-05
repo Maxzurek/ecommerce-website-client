adb27ffdf149b6c8afe2c5c5646b8de556af74a1
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-05-05 17:52:23 -0400

    Add Router outlet wrapper

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,21 @@
+import "./RouterOutlet.scss";
+
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import Home from "./routes/home/Home";
 import Shop from "./routes/shop/Shop";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import AboutUs from "./routes/aboutUs/AboutUs";
+import Contact from "./routes/contact/Contact";
 
 export const router = createBrowserRouter([
     {
         element: (
             <>
                 <Header />
-                <Outlet />
+                <div className="router-outlet">
+                    <Outlet />
+                </div>
                 <Footer />
             </>
         ),
@@ -21,6 +27,14 @@ export const router = createBrowserRouter([
             {
                 path: "/shop",
                 element: <Shop />
+            },
+            {
+                path: "/about-us",
+                element: <AboutUs />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
             }
         ]
     }
