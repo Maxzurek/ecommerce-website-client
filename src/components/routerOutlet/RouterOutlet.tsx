c9782f672634d978feb0f66d8654916898cac2eb
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-05-06 00:45:31 -0400

    Update RouterOutlet  - Add navigation animation

--- src/components/routerOutlet/RouterOutlet.tsx
+++ src/components/routerOutlet/RouterOutlet.tsx
@@ -1,10 +1,34 @@
+import { useLayoutEffect, useState } from "react";
 import "./RouterOutlet.scss";
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { withClassNames } from "../../utilities/WithClassNames";
+import { usePrevious } from "../../hooks/usePrevious";
 
 const RouterOutlet = () => {
+    const [animateNavigation, setAnimateAnimation] = useState(true);
+
+    const { pathname } = useLocation();
+    const previousPathName = usePrevious(pathname);
+
+    useLayoutEffect(() => {
+        if (pathname !== previousPathName) {
+            setAnimateAnimation(true);
+        }
+    }, [pathname, previousPathName]);
+
+    const handleAnimationEnd = () => {
+        setAnimateAnimation(false);
+    };
+
     return (
-        <div className="router-outlet">
+        <div
+            className={withClassNames([
+                "router-outlet",
+                animateNavigation && "router-outlet--animate-navigation"
+            ])}
+            onAnimationEnd={handleAnimationEnd}
+        >
             <Outlet />
         </div>
     );
