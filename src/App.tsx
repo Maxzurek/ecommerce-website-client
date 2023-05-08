16fb39512a9db6c1fa59242618bc42df9996d7d6
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-05-08 17:06:17 -0400

    Update App  - Add router variable and remove Router.tsx  - Rename font global variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
 import "./App.scss";
 
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Router";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ScrollToTopButton from "./components/scrollTopButton/ScrollToTopButton";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import RouterOutlet from "./components/routerOutlet/RouterOutlet";
+import AboutUs from "./routes/aboutUs/AboutUs";
+import Contact from "./routes/contact/Contact";
+import Home from "./routes/home/Home";
+import Shop from "./routes/shop/Shop";
 
 const App = () => {
+    const router = createBrowserRouter([
+        {
+            element: (
+                <>
+                    <Header />
+                    <RouterOutlet />
+                    <Footer />
+                </>
+            ),
+            children: [
+                {
+                    path: "/",
+                    element: <Home />
+                },
+                {
+                    path: "/shop",
+                    element: <Shop />
+                },
+                {
+                    path: "/about-us",
+                    element: <AboutUs />
+                },
+                {
+                    path: "/contact",
+                    element: <Contact />
+                },
+                {
+                    path: "/product-page/:productId",
+                    element: <div>TODO</div>
+                }
+            ]
+        }
+    ]);
+
     return (
         <div className="app">
             <RouterProvider router={router} />
