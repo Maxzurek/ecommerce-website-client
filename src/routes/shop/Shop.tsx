ef222378d871febec9cd866fbf942d00e5c5d7e3
Maxzurek <74398804+Maxzurek@users.noreply.github.com>
2023-05-12 11:36:36 -0400

    Add Shop

--- src/routes/shop/Shop.tsx
+++ src/routes/shop/Shop.tsx
@@ -1,7 +1,43 @@
 import "./Shop.scss";
 
-const Shop = () => {
-    return <div className="shop">Shop</div>;
+import { products } from "../../Products";
+import ProductCard from "../../components/products/ProductCard";
+import ProductList from "../../components/products/ProductList";
+import { Product } from "../../interfaces/Product.interfaces";
+
+interface ShopProps {
+    onShowAddToCartDialog: (product: Product) => void;
+}
+
+const Shop = ({ onShowAddToCartDialog }: ShopProps) => {
+    const newArrivals = products.men.shirt.filter((product) => product.isNew);
+
+    return (
+        <div className="shop">
+            <span className="shop__header">NEW ARRIVALS</span>
+            <ProductList>
+                {newArrivals.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        isNew
+                        product={product}
+                        onShowAddToCartDialog={onShowAddToCartDialog}
+                    />
+                ))}
+            </ProductList>
+            <span className="shop__header">SHOP ALL</span>
+            <ProductList>
+                {products.men.shirt.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        isNew={product.isNew}
+                        product={product}
+                        onShowAddToCartDialog={onShowAddToCartDialog}
+                    />
+                ))}
+            </ProductList>
+        </div>
+    );
 };
 
 export default Shop;
